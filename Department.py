f45ff074a75070dc2caa123c259a4cb52bb07e9e
KinshukGanguly <121783408+KinshukGanguly@users.noreply.github.com>
2023-01-03 05:19:42 +0530

    Add files via upload

---
+++ Department.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import  Exam as ex
+from matplotlib import pyplot
+import numpy as np
+def Create_Department():
+    name=input('Enter the department name')
+    id=input("Enter department ID")
+
+    data={'Department ID':[id],'Department Name':[name],'List of Batches':[""]}
+    df = pd.DataFrame(data)
+    df.to_csv("Department.csv",mode='a',index=False,header=False)
+    print("The department has been created.The list of batches will be updated as and when the batches get created with this particular department id")
+
+def View_Batches():
+    id=input("Enter department ID")
+    dfd=pd.read_csv("Department.csv")
+    for i in range(len(dfd)):
+        if dfd.iat[i,0]==id:
+            st=dfd.iat[i,2]
+            break
+    batch_list=st.split(":")
+    print(str(batch_list))
+    '''dfb=pd.read_csv("Batch.csv")
+    print("LIST OF BATCHES UNDER THIS DEPARTMENT-")
+    for i in range(len(batch_list)):
+        batch_ID=batch_list[i]
+        for j in range(len(dfb)):
+            if dfb.iat[j,0]==batch_ID:
+                print(batch_ID+"\t\t"+dfb.iat[j,2])
+                break #for j'''
+
+
+def View_Batch_Performance():
+    dept_ID=input('Enter department ID')
+
+    batch_studentList=[]
+    dfb=pd.read_csv("Batch.csv")
+    b_avg = []
+    b_name = []
+    course_avg=0
+    batch_avg=0
+
+    for i in range(len(dfb)):
+        if dfb.iat[i,2]==dept_ID:
+            batch_studentList.append([dfb.iat[i,0],dfb.iat[i,1],dfb.iat[i,3],dfb.iat[i,4]])
+
+    for i in range(len(batch_studentList)):
+        batch_detail=batch_studentList[i]
+        batch_ID=batch_detail[0]
+        batch_name=batch_detail[1]
+        b_name.append(batch_name)
+        print("BATCH NAME-"+batch_name)
+        print("BATCH ID-" +batch_ID )
+
+        course_list=batch_detail[2].split(":")
+        #student_list=batch_detail[3].split(":")
+        for j in range(len(course_list)):
+            courseID=course_list[j]
+
+            course_avg+=ex.average(batch_ID,courseID)
+
+        batch_avg=course_avg/len(course_list)
+        b_avg.append(batch_avg)
+        course_avg=0
+        print("BATCH AVERAGE="+str(batch_avg)+"\n")
+
+def department_statistics():
+    dept_ID = input('Enter department ID')
+    batch_studentList = []
+    dfb = pd.read_csv("Batch.csv")
+
+    course_avg = 0
+
+    b_name=[]
+    b_avg=[]
+    for i in range(len(dfb)):
+        if dfb.iat[i, 2] == dept_ID:
+            batch_studentList.append([dfb.iat[i,0],dfb.iat[i,1],dfb.iat[i,3]])
+
+
+    for i in range(len(batch_studentList)):
+        batch_detail = batch_studentList[i]
+        batch_ID = batch_detail[0]#taking each batch id at a time
+
+        b_name.append(batch_detail[1])#storing  batch names under this department
+        course_list = batch_detail[2].split(":")
+        # student_list=batch_detail[3].split(":")
+        for j in range(len(course_list)):
+            courseID = course_list[j]
+            course_avg += ex.average(batch_ID, courseID)
+
+        batch_avg = course_avg / len(course_list)
+        b_avg.append(batch_avg)#storing the average marks of the batches
+        course_avg = 0
+
+    pyplot.plot(b_name,b_avg)
+    pyplot.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Student.py
@@ -0,0 +1,185 @@
+import pandas as pd
+
+
+def create_student(*args):
+    if len(args)==0:
+        id = input("Enter student ID-")
+        name = input("Enter student name-")
+        roll = input("Enter class roll number-")
+        batch = input("Enter batch ID-")
+    else:
+        id=args[0]
+        name=args[1]
+        roll=args[2]
+        batch=args[3]
+
+    data = {'Student ID': [id], 'Name': [name], 'Class Roll Number': [roll],'Batch ID':[batch]}
+    df = pd.DataFrame(data)
+    df.to_csv("Student.csv", mode='a', index=False, header=False)
+    df2=pd.read_csv("Batch.csv")
+    flag=1
+    for i in range(len(df2)):
+        if (str(df2.iat[i, 0])) == batch:
+            flag=0
+            st=str(df2.iat[i, 4])#extracting the list of existing students
+            if id in st:#already id exists
+              break
+            else:
+                if st=="nan":#empty string
+                    st=id
+                else:
+                    st=st+":"+id#adding new id to existing string
+                    df2.iat[i, 4]=st#updating string
+                break
+    df2.to_csv("Batch.csv", index=False)
+
+    if flag==1:
+        print("The entered batch ID doesn't exist")
+    print("Student record updated successfully")
+
+
+def update_student():
+    id=input("Enter the student id whose record you want to update")
+    df=pd.read_csv("Student.csv")
+    for i in range(len(df)):
+        if (str(df.iat[i, 0])) == id:
+            choice=int(input("Enter 1 for changing name,2 for changing roll no,3 for changing batch ID"))
+            data=input("enter the new data to be updated")
+            if choice==3:
+                name = df.iat[i, 1]
+                roll = df.iat[i, 2]
+                remove_student(id)
+                create_student(id,name,roll,data)
+            else:
+                df.iat[i, choice] = data
+
+
+
+    df.to_csv("Student.csv",index=False)
+
+
+
+
+def remove_student(*args):
+    if len(args)==0:
+        id = input("ENTER STUDENT ID")
+    else:
+        id=args[0]
+
+    print(type(id))
+    df=pd.read_csv("Student.csv")
+    for i in range(len(df)):
+       if (str(df.iat[i,0]))==id:
+           batch=df.iat[i,3]
+           print("RECORD DELETED")
+           df = df.drop(i)
+           break
+    df2 = pd.read_csv("Batch.csv")
+    flag = 1
+    for i in range(len(df2)):
+        if (str(df2.iat[i, 0])) == batch:
+            flag = 0
+            st = str(df2.iat[i, 4])
+            # extracting the list of existing students
+            st=st.split(":")
+            if id in st:  # already id exists
+                st.remove(id)
+                string=st[0]
+                for i in range(1,len(st)):
+                    string=string+":"+st[i]
+
+                df2.iat[i, 4]=string
+
+
+    df2.to_csv("Batch.csv", index=False)
+    if flag == 1:
+        print("The entered batch ID doesn't exist")
+    df.to_csv("Student.csv",index=False)
+
+def Report_Card():
+    id=input("Enter Student ID")
+    dfs=pd.read_csv("Student.csv")
+    dfb=pd.read_csv("Batch.csv")
+    dfc=pd.read_csv("Course.csv")
+
+    marks=[]
+    course_ID=[]
+    course=[]
+    name=""
+    roll=0
+    total=0
+    batch_ID=""
+    for i in range(len(dfs)):
+        if dfs.iat[i,0]==id:
+            name=dfs.iat[i,1]
+            roll=dfs.iat[i,2]
+            batch_ID=dfs.iat[i,3]
+            break
+
+    for i in range(len(dfb)):
+        if dfb.iat[i,0]==batch_ID:
+            st=dfb.iat[i,3]
+            course_ID=st.split(":")
+            #list of course IDs for that batch
+            break
+    for i in range(len(course_ID)):
+        courseID=course_ID[i]
+
+        #taking all courses one by one that are in batch
+        for j in range(len(dfc)):
+            if dfc.iat[j,0]==courseID:#searching for the course
+                course.append(dfc.iat[j,1])
+
+                m = str(dfc.iat[j, 2]).split("-")
+
+                #student-marks pair list for that course
+                for k in range(len (m)):
+                    sm=m[k].split(":")
+
+                    if sm[0]==id:
+                        marks.append(int(sm[1]))
+
+
+    f=open("Result.txt","w")
+    f.write("NAME-"+name+'\n')
+    f.write("STUDENT ID-"+id+'\n')
+    f.write("BATCH ID-" + batch_ID+'\n')
+    f.write("ROLL NUMBER-" + str(roll)+'\n')
+    print("\n")
+    for i in range(len(course)):
+        total+=marks[i]
+        list1=grade(marks[i])#list
+        f.write("Course="+course[i]+"("+course_ID[i]+")"+"\t"+"marks="+str(marks[i])+'\t'+"Grade="+list1[0]+"\t\t"+"Pass/Fail status="+list1[1]+'\n')
+
+    total=float(total)/ len(marks)
+    list2=grade(total)
+    print("\n")
+    
+    f.write("OVER ALL GRADE="+str(list2[0])+'\n')
+    f.write("PASS STATUS=" + str(list2[1])+'\n')
+    f.close()
+    print("The result shall be created in the same directory as the database")
+
+
+def grade(marks):
+    if marks >= 90:
+        grade = 'A'
+    elif marks >= 80:
+        grade = 'B'
+    elif marks >= 70:
+        grade = 'C'
+    elif marks >= 60:
+        grade = 'D'
+    elif marks >= 50:
+        grade = 'E'
+    else:
+        return 'F', 'Failed'
+    return (grade, 'Passed')
+
+
+
+
+
+
+
+
